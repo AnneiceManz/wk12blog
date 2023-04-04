9a9b4909cd9131c99d28f7b2ca7d490f3c254946
Anneice Manzanares <mountains.stars@gmail.com>
2023-04-04 12:18:51 -0600

    created topbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
+import TopBar from './components/TopBar';
 
 
 function App() {
 
   return (
     <div className="App">
-      <MyNavBar />
-      <ListStudents />
+      <TopBar />
 
     </div>
   )
