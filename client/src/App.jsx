ef8630419af43ae295f82891a2b6d2baaed4a3e9
Anneice Manzanares <mountains.stars@gmail.com>
2023-04-04 12:23:50 -0600

    created header and sidecar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TopBar from './components/TopBar';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopBar from "./components/TopBar";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-
   return (
     <div className="App">
       <TopBar />
-
+      <Header />
+      <Sidebar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
