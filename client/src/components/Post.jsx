6792f956aedabb1743382fdf939161539a8db25e
Anneice Manzanares <mountains.stars@gmail.com>
2023-04-06 11:45:53 -0600

    connected posts to front end

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,37 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Post = ({ img }) => {
+const Post = ({ post }) => {
   return (
     <div className="post">
-      <img className="postImg" src={img} alt="" />
+      {/* <img className="postImg" src={} alt="" /> */}
       <div className="postInfo">
-        <div className="postCats">
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Music
-            </Link>
-          </span>
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Life
-            </Link>
-          </span>
-        </div>
-        <span className="postTitle">
-          <Link to="/post/abc" className="link">
-            Lorem ipsum dolor sit amet
+        {/* <div className="postCats">
+          {post.categories.map((category) => (
+
+          <span className="postCat">{category.name}</span>
+          ))}
+        </div> */}
+          <Link to={`/post/${post.post_id}`} className="link">
+        <span className="postTitle">{post.title}</span>
           </Link>
-        </span>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">{new Date(post.posted).toDateString()}</span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-        atque, exercitationem quibusdam, reiciendis odio laboriosam?
-      </p>
+      <p className="postDesc">{post.post_text}</p>
     </div>
   );
 };
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Posts from "../components/Posts";
 import Sidebar from "../components/Sidebar";
-import { Outlet } from "react-router-dom";
 
 const Homepage = () => {
   const location = useLocation();
   console.log(location);
+
+  const [blogPosts, setBlogPosts] = useState([]);
+
+
+  const loadBlogPosts = () => {
+      // A function to fetch the list of students that will be load anytime that list change
+      fetch("http://localhost:8080/api/posts")
+          .then((response) => response.json())
+          .then((blogPosts) => {
+              setBlogPosts(blogPosts);
+          });
+  }
+
+  useEffect(() => {
+      loadBlogPosts();
+  }, [blogPosts]);
+
   return (
     <div>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={blogPosts} />
         <Sidebar />
       </div>
     </div>
